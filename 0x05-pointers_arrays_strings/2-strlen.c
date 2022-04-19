135e582adb0169d3d2a63d24578f7592ec4d9c24
Meba316 <yifrutsi@gmail.com>
2022-04-19 01:15:43 -0700

    my main

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -4,15 +4,15 @@
  *_strlen - length of a string
  *@s: A pointer to an int that will be changed/uppdated
  *
- *Return: void thatmeas answer is correct
+ *Return: void that means answer is correct
  */
 
-int _strlen(char *s)
+int _strlen (char *s)
 {
 	int u;
 
 	u = 0;
-	while(s[u] != '\n')
+	while (s[u] != '\n')
 	{
 		u++;
 	}
